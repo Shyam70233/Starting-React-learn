e6e11ec0c5e3682792c695f85c0e8bac7e9a91c6
Shyam70233 <deluxshop468@gmail.com>
2022-05-29 00:28:18 +0530

    c22

--- src/C1.js
+++ src/C1.js
@@ -1,15 +1,28 @@
 import React, { useState } from 'react';
 function C1() {
   // console.log(useState('RadheShyam'));
-  const [name, setname] = useState('Radhe');
-  const onchaneName = () => {
-    setname('Shyamk');
+  const [x, setVal] = useState(0);
+  const p1 = () => {
+    setVal(x + 1);
   };
+  const p2 = () => {
+    setVal(x - 1);
+  };
+  const p3 = () => {
+    setVal(0);
+  };
+  const p4 = () => {
+    setVal(x + 200);
+  };
+
   return (
     <>
-      <h1>1st Work</h1>
-      <div>My Name is: {name} </div>
-      <button onClick={onchaneName}>clic to Chnage Name</button>
+      <h1>2nd Work</h1>
+      <h1>X : {x}</h1>
+      <button onClick={p1}>+1</button>
+      <button onClick={p2}>-1</button>
+      <button onClick={p3}>0</button>
+      <button onClick={p4}>200</button>
     </>
   );
 }
