50ec5bd3677eef88f32168e212e097f1db07f43d
Shyam70233 <deluxshop468@gmail.com>
2022-05-29 14:27:17 +0530

    program 3

--- src/C1.js
+++ src/C1.js
@@ -1,67 +1,31 @@
-import React, { useState } from 'react';
+import React, { useRef } from 'react';
 // import User from './Users';
 function C1() {
-  const [form, setform] = useState({
-    name: '',
-    emails: '',
-    phone: '',
-    password: '',
-    c: '',
-    otp: '',
-  });
+  const ref1 = useRef();
+  const ref2 = useRef();
+  const handlechange = () => {
+    console.log(ref1.current);
+    console.log(ref2.current);
+    ref1.current.rows = 10;
+    ref1.current.cols = 20;
+    ref1.current.disabled = true;
+    ref1.current.placeholder = 'Enter Message';
+    ref1.current.style.color = 'red';
+    ref1.current.style.background = 'yellow';
+    ref1.current.style.border = '5px solid black';
 
-  console.log('Radhe', form);
-  const handlechange = (e) => {
-    // console.log(e.target.placeholder, e.target.value);
-    setform({ ...form, [e.target.placeholder]: e.target.value });
+    ref2.current.type = 'date';
+    ref2.current.min = '2022-05-20';
+    ref2.current.max = '2022-05-30';
+    ref2.current.value = '2022-05-17';
   };
-  const inputStyle = {
-    display: 'block',
-    margin: 15,
-  };
+
   return (
     <>
       <h1>Users Table </h1>
-      <input
-        style={inputStyle}
-        placeholder="name"
-        value={form.name}
-        onChange={handlechange}
-      />
-
-      <input
-        style={inputStyle}
-        placeholder="emails"
-        value={form.emails}
-        onChange={handlechange}
-      />
-
-      <input
-        style={inputStyle}
-        placeholder="phone"
-        value={form.phone}
-        onChange={handlechange}
-      />
-      <input
-        style={inputStyle}
-        placeholder="password"
-        value={form.password}
-        onChange={handlechange}
-      />
-      <input
-        style={inputStyle}
-        placeholder="c"
-        value={form.c}
-        onChange={handlechange}
-      />
-      <input
-        style={inputStyle}
-        placeholder="otp"
-        value={form.otp}
-        onChange={handlechange}
-      />
-      <button>Click Me</button>
-      {JSON.stringify(form)}
+      <textarea ref={ref1} />
+      <input ref={ref2} />
+      <button onClick={handlechange}>click me</button>
     </>
   );
 }
